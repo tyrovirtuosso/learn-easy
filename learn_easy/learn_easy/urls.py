a38876c6728c2992f17d20cbd2e11a36831a40eb
tyrovirtuosso <tyrovirtuosso@proton.me>
2023-09-17 15:48:22 -0400

    removed unecessary templates and code

--- learn_easy/learn_easy/urls.py
+++ learn_easy/learn_easy/urls.py
@@ -6,7 +6,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("admin/", admin.site.urls),
-    path("users/", include("usersApp.urls")),
-    path("users/", include("django.contrib.auth.urls")),
-    path("accounts/", include("allauth.urls")),  # This line should include allauth.urls under accounts/
+    # path("users/", include("usersApp.urls")),
+    # path("users/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")), 
 ]
--- learn_easy/usersApp/views.py
+++ learn_easy/usersApp/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from .forms import CustomUserCreationForm
 
 
-class SignUp(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+# class SignUp(generic.CreateView):
+#     form_class = CustomUserCreationForm
+#     success_url = reverse_lazy("login")
+#     template_name = "registration/signup.html"
