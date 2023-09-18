416d6bc028a065351f03a9da462b84fbdde6b0bb
tyrovirtuosso <tyrovirtuosso@proton.me>
2023-09-18 16:42:56 -0400

    added async support for generating meanings. Now users can move on to saving the next word without needing to wait for meaning and category generation of the previous word

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learn-easy
 Learn anything, easily with a supercharged Anki, that incorporates GPT flawlessly!
+
+To run:
+uvicorn learn_easy.asgi:application --reload
+or
+python manage.py runserver
--- learn_easy/items/views.py
+++ learn_easy/items/views.py
@@ -5,6 +5,11 @@ from django.contrib.auth.decorators import login_required
 from openai_API.api import OpenAI_API
 from django.contrib import messages
 
+from threading import Thread
+
+ai = OpenAI_API()
+
+
 @login_required
 def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -20,30 +25,18 @@ def create_item(request):
     if request.method == 'POST':
         form = ItemForm(request.POST)
         if form.is_valid():
-            ai = OpenAI_API()
-
             item = form.save(commit=False)
             item.user = request.user
             
-            # Get the word from the form
             word = form.cleaned_data['word']
-            
-            # Apply spelling correction
             corrected_word = ai.spelling_corrector([word])[0]
-            
-            # Detect Category
-            detected_category = ai.get_category(corrected_word)
-            
-            print("getting meaning\n")
-            meaning = ai.get_meaning(corrected_word, detected_category)
-            
             item.word = corrected_word
-            item.category = detected_category
-            item.meaning = meaning
-            
             item.save()
-            
             messages.success(request, 'Item saved successfully.')
+            
+            # Start thread to populate meaning
+            thread = Thread(target=get_meaning, args=(item, corrected_word))
+            thread.start()            
             
             # Redirect to the same page
             return redirect('items:create_item')
@@ -56,3 +49,10 @@ def delete_item(request, pk):
     item = Item.objects.get(pk=pk)
     item.delete()
     return redirect('items:item_list')
+
+def get_meaning(item, corrected_word):
+    detected_category = ai.get_category(corrected_word)
+    meaning = ai.get_meaning(corrected_word, detected_category)
+    item.category = detected_category
+    item.meaning = meaning
+    item.save() 
