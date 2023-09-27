2323cf9c69993571677c771b952e941c5caecc10
tyrovirtuosso <tyrovirtuosso@proton.me>
2023-09-27 18:00:19 -0400

    added new signal that creates a default deck as soon as the user is created

--- learn_easy/cards/signals.py
+++ learn_easy/cards/signals.py
@@ -1,22 +1,10 @@
 from django.db.models.signals import post_save, pre_save, post_migrate
 from django.dispatch import receiver
-from .models import Card, Level
 from .scripts.add_levels import add_levels
 
-from decks.models import Deck, DeckCard
-
-@receiver(post_save, sender=Card)
-def create_default_deck_for_new_card(sender, instance, created, **kwargs):
-    if created:
-        # Check if the user already has a "default" deck
-        default_deck, created = Deck.objects.get_or_create(
-            user=instance.user,
-            name="Default",
-        )
-        # Check if the card is already in the default deck
-        if not DeckCard.objects.filter(deck=default_deck, card=instance).exists():
-            # Add the card to the default deck
-            DeckCard.objects.create(deck=default_deck, card=instance)
+from decks.models import Deck
+from cards.models import Card, Level
+from usersApp.models import CustomUser
 
 @receiver(pre_save, sender=Card)
 def set_default_level(sender, instance, **kwargs):
@@ -25,7 +13,11 @@ def set_default_level(sender, instance, **kwargs):
         level_one = Level.objects.get(level_number=1)
         instance.level = level_one
 
-
 @receiver(post_migrate)
 def create_levels(sender, **kwargs):
     add_levels()
+
+@receiver(post_save, sender=CustomUser)
+def create_default_deck(sender, instance, created, **kwargs):
+    if created:
+        Deck.objects.create(name='default', user=instance)
