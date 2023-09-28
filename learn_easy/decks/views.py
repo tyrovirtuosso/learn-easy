c22c8ac6f474faab4a4eb19ebcf96bb9a28714db
tyrovirtuosso <tyrovirtuosso@proton.me>
2023-09-28 19:30:32 -0400

    enabled the option for cards to be added or removed from a deck.

--- learn_easy/decks/views.py
+++ learn_easy/decks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Deck
+from cards.models import Card
 from .forms import DeckForm
 from django.contrib.auth.decorators import login_required 
 from django.shortcuts import render, redirect, get_object_or_404
@@ -34,4 +35,34 @@ def deck_list(request):
 def deck_detail(request, deck_id):
     deck = get_object_or_404(Deck, id=deck_id, user=request.user)  
     cards = deck.cards.all()  # related_name='cards' in Cards to decks ManyToManyField
-    return render(request, 'deck/deck_detail.html', {'deck': deck, 'cards': cards})
+    available_cards = Card.objects.exclude(decks=deck)
+    is_default_deck = deck.deck_name == 'default'
+    return render(request, 'deck/deck_detail.html', {'deck': deck, 'cards': cards, 'available_cards': available_cards, 'is_default_deck': is_default_deck})
+
+
+# View to add a card to the deck
+def add_card_to_deck(request, deck_id):
+    if request.method == 'POST':
+        deck = get_object_or_404(Deck, id=deck_id)
+        card_pk = request.POST.get('add_card')
+        card = get_object_or_404(Card, pk=card_pk)
+        deck.cards.add(card)
+    return redirect('decks:deck_detail', deck_id=deck_id)
+
+# View to remove a card from the deck
+def remove_card_from_deck(request, deck_id, card_pk):
+    deck = get_object_or_404(Deck, pk=deck_id)
+    card = get_object_or_404(Card, pk=card_pk)
+    
+    # Check if the deck is the 'default' deck
+    is_default_deck = deck.deck_name == 'default'
+    
+    if request.method == 'POST':
+        if is_default_deck:
+            # Remove the card from the database
+            card.delete()
+        else:
+            # Remove the card only from the specific deck
+            deck.cards.remove(card)
+            
+    return redirect('decks:deck_detail', deck_id=deck_id)
